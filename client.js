d17531a5640b31d6262206851eabba2e7a3cca23
keefcreative <khodgetts@keefcreative.com>
2025-03-24 22:31:18 +0000

    dd

--- client.js
+++ client.js
@@ -1,21 +1,19 @@
 const Promise = window.TrelloPowerUp.Promise;
 
-function openModal(t) {
-  return t.modal({
-    url: './index.html',
-    accentColor: '#0079BF',
-    height: 400,
-    fullscreen: false,
-    title: 'AI Assistant'
-  });
-}
-
 window.TrelloPowerUp.initialize({
   'card-buttons': function(t, options) {
     return [{
       icon: 'https://cdn-icons-png.flaticon.com/512/4712/4712104.png',
       text: 'AI Assistant',
-      callback: openModal
+      callback: function(t) {
+        return t.modal({
+          url: './index.html',
+          accentColor: '#0079BF',
+          height: 400,
+          fullscreen: false,
+          title: 'AI Assistant'
+        });
+      }
     }];
   }
 });
