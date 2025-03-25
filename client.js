c545463d03a8d9b411f19bd3aa5bef59d75539c4
keefcreative <khodgetts@keefcreative.com>
2025-03-25 09:09:17 +0000

    simplify

--- client.js
+++ client.js
@@ -1,35 +1,25 @@
-const t = window.TrelloPowerUp.iframe();
-
 window.TrelloPowerUp.initialize({
-  'card-buttons': function(t) {
+  'card-buttons': function(t, options){
     return [{
-      icon: {
-        dark: 'https://cdn-icons-png.flaticon.com/512/4712/4712104.png',
-        light: 'https://cdn-icons-png.flaticon.com/512/4712/4712104.png'
-      },
+      icon: 'https://cdn-icons-png.flaticon.com/512/4712/4712104.png',
       text: 'AI Assistant',
-      callback: function(t) {
+      callback: function(t){
         return t.modal({
           url: './index.html',
-          height: 400,
-          fullscreen: false,
+          height: 500,
           title: 'AI Assistant'
         });
       }
     }];
   },
-  'board-buttons': function(t) {
+  'board-buttons': function(t, options){
     return [{
-      icon: {
-        dark: 'https://cdn-icons-png.flaticon.com/512/4712/4712104.png',
-        light: 'https://cdn-icons-png.flaticon.com/512/4712/4712104.png'
-      },
+      icon: 'https://cdn-icons-png.flaticon.com/512/4712/4712104.png',
       text: 'AI Assistant',
-      callback: function(t) {
+      callback: function(t){
         return t.modal({
           url: './board-modal.html',
           height: 500,
-          fullscreen: false,
           title: 'AI Assistant Board Tools'
         });
       }
